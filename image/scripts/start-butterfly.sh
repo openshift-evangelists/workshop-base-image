523972e8a9c642507b3ab7d8d64e3d8ed37df567
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-09-02 15:34:16 +1000

    No longer need to create empty config file.

--- image/scripts/start-butterfly.sh
+++ image/scripts/start-butterfly.sh
@@ -6,13 +6,6 @@ set -x
 
 URI_ROOT_PATH=${URI_ROOT_PATH:-}
 
-# The butterfly application has a bug whereby if the config file is
-# not present, it will ignore the --uri-root-path option, so create
-# the file if it doesn't exist.
-
-mkdir -p $HOME/.config/butterfly
-touch $HOME/.config/butterfly/butterfly.conf
-
 # Now execute the program. We need to supply a startup script for the
 # shell to setup the environment.
 
