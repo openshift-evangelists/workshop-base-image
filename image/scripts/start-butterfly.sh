cf800cb95ecfc7cf38f2d296546c8fb0bf805b7e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-09-02 15:40:24 +1000

    Make environment variables specific to butterfly.

--- image/scripts/start-butterfly.sh
+++ image/scripts/start-butterfly.sh
@@ -4,7 +4,8 @@ set -eo pipefail
 
 set -x
 
-URI_ROOT_PATH=${URI_ROOT_PATH:-}
+BUTTERFLY_URL_PREFIX=${BUTTERFLY_URL_PREFIX:-}
+BUTTERFLY_SERVER_PORT=${BUTTERFLY_SERVER_PORT:-8080}
 
 # Now execute the program. We need to supply a startup script for the
 # shell to setup the environment.
@@ -19,7 +20,7 @@ if [ -f /opt/app-root/etc/motd ]; then
     MOTD_FILE=/opt/app-root/etc/motd
 fi
 
-exec /opt/terminal/bin/butterfly.server.py --port=8080 \
-    --host=0.0.0.0 --uri-root-path="$URI_ROOT_PATH" --unsecure \
+exec /opt/terminal/bin/butterfly.server.py --port=$BUTTERFLY_SERVER_PORT \
+    --host=0.0.0.0 --uri-root-path="$BUTTERFLY_URL_PREFIX" --unsecure \
     --i-hereby-declare-i-dont-want-any-security-whatsoever \
     --shell=/opt/terminal/bin/start-terminal.sh --motd=$MOTD_FILE
