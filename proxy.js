1decc3ed22f25cd42c98840852d86a1469a60c18
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-09-08 14:15:24 +1000

    Add HTTP Basic authentication support.

--- proxy.js
+++ proxy.js
@@ -8,6 +8,9 @@ var terminal_app = 'http://127.0.0.1:8082';
 var terminal_top = process.env.URI_ROOT_PATH || '/user/default'
 var terminal_url = '^' + terminal_top + '/terminal/.*$';
 
+var terminal_user = process.env.TERMINAL_USER
+var terminal_pass = process.env.TERMINAL_PASS
+
 console.log('TERMINAL_URL' + terminal_url);
 
 var proxy = httpProxy.createProxyServer({});
@@ -21,6 +24,33 @@ function on_error(err, req, res) {
 }
 
 var server = http.createServer(function(req, res) {
+  var auth = req.headers['authorization'];
+
+  if (terminal_user) {
+    if (!auth) {
+	res.statusCode = 401;
+	res.setHeader('WWW-Authenticate', 'Basic realm="Terminal"');
+
+	res.end('Login required.');
+    }
+    else {
+      var tmp = auth.split(' ');
+      var buf = new Buffer(tmp[1], 'base64');
+      var plain_auth = buf.toString();
+
+      var creds = plain_auth.split(':');
+      var username = creds[0];
+      var password = creds[1];
+
+      if ((username != terminal_user) || (password != terminal_pass)) {
+	res.statusCode = 401;
+	res.setHeader('WWW-Authenticate', 'Basic realm="Terminal"');
+
+	res.end('Access denied.');
+      }
+    }
+  }
+
   var target_app = workshop_app;
 
   var parsed_url = url.parse(req.url);
